af0cb08ee33a00e3cb7b119db44874a2f0e2b138
Alexandru Ionita <alexandru.ionita@gmail.com>
2017-05-24 06:36:53 +0200

    Initial commit

---
+++ src/main/java/ProcesResourcesExp.java
@@ -0,0 +1,11 @@
+/**
+ * @author alexandru.ionita
+ * @since 1.0.0
+ */
+public class ProcesResourcesExp
+{
+    public static void main(String[] args)
+    {
+        System.out.println("this is a test");
+    }
+}
